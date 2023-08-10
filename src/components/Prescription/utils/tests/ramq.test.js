3419bf5b7a18618d0dd9bcfdac672e64d496abdf
claudia1296 <claudia.roy.cr@gmail.com>
2023-08-10 14:01:56 -0400

    fix: CLIN-1463 ajout test

--- src/components/Prescription/utils/tests/ramq.test.js
+++ src/components/Prescription/utils/tests/ramq.test.js
@@ -5,5 +5,19 @@ describe('extract ramq', () => {
     expect(extractBirthDateAndSexFromRamq('TEST55072223', 'yyyy-MM-dd').birthDate).toEqual(
       '1955-07-22',
     );
+    expect(extractBirthDateAndSexFromRamq('TEST23581023', 'yyyy-MM-dd').birthDate).toEqual(
+      '2023-08-10',
+    );
+  });
+
+  test('should return invalid date', () => {
+    expect(extractBirthDateAndSexFromRamq('TEST23701023', 'yyyy-MM-dd').birthDate).toEqual(
+      undefined,
+    );
+  });
+
+  test('should return the good sex', () => {
+    expect(extractBirthDateAndSexFromRamq('TEST55072223', 'yyyy-MM-dd').sex).toEqual('male');
+    expect(extractBirthDateAndSexFromRamq('TEST55572223', 'yyyy-MM-dd').sex).toEqual('female');
   });
 });
