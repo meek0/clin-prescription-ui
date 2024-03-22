146aa072c44f39653796924853641940719644d2
meek0 <cedricgfont@gmail.com>
2024-03-22 09:42:33 -0400

    feat: PRESC-153 add org reference afeter patient id

--- src/views/Prescriptions/Detail/PatientCard/index.tsx
+++ src/views/Prescriptions/Detail/PatientCard/index.tsx
@@ -14,7 +14,10 @@ interface OwnProps {
 const PatientCard = ({ prescription, loading }: OwnProps) => (
   <Card title={intl.get('screen.prescription.entity.patient.card.title')} data-cy="PatientCard">
     <ParagraphLoader loading={loading} paragraph={{ rows: 6 }}>
-      <PatientContent patient={prescription?.subject.resource!} />
+      <PatientContent
+        patient={prescription?.subject.resource!}
+        reference={prescription?.requester?.organization?.reference}
+      />
     </ParagraphLoader>
   </Card>
 );
