7b4b8ee3001d7a4c609f54d25b1b51c0020664e3
meek0 <cedricgfont@gmail.com>
2024-04-12 09:20:11 -0400

    fix: PRESC-240 check token expiration on switching steps

--- src/auth/keycloak.ts
+++ src/auth/keycloak.ts
@@ -2,7 +2,7 @@ import Keycloak from 'keycloak-js';
 
 import { keycloakConfig } from 'utils/config';
 
-const keycloak = Keycloak(keycloakConfig);
+const keycloak = new Keycloak(keycloakConfig);
 
 export const getFhirPractitionerId = () =>
   keycloak.tokenParsed ? keycloak.tokenParsed.fhir_practitioner_id : undefined;
