e3ba22e99ea408bc5b5bf74f355817105a3504bf
meek0 <cedricgfont@gmail.com>
2024-04-29 07:09:45 -0400

    fix: PRESC-152 add foetus info

--- src/views/Prescriptions/Detail/PatientCard/PatientContent/index.tsx
+++ src/views/Prescriptions/Detail/PatientCard/PatientContent/index.tsx
@@ -1,19 +1,68 @@
 import intl from 'react-intl-universal';
-import { Descriptions } from 'antd';
-import { extractOrganizationId, extractPatientId } from 'api/fhir/helper';
+import { Descriptions, Divider, Typography } from 'antd';
+import { extractObservationId, extractOrganizationId, extractPatientId } from 'api/fhir/helper';
 import { PatientServiceRequestFragment } from 'api/fhir/models';
 import { formatName, formatRamq } from 'api/fhir/patientHelper';
+import { useGestationalDateObservationEntity } from 'graphql/prescriptions/actions';
 
 import { EMPTY_FIELD } from 'components/Prescription/Analysis/AnalysisForm/ReusableSteps/constant';
+import { calculateGestationalAgeFromDDM, calculateGestationalAgeFromDPA } from 'utils/age';
 import { formatDate } from 'utils/date';
 
 interface OwnProps {
   patient: PatientServiceRequestFragment;
   reference?: string;
+  isPrenatal?: boolean;
   labelClass?: 'label-20' | 'label-25' | 'label-35';
 }
 
-const PatientContent = ({ patient, reference, labelClass = 'label-35' }: OwnProps) => {
+const { Text } = Typography;
+
+const DDM = '8665-2';
+const DPA = '11778-8';
+
+const getGestationalAge = (valueDateTime: string, code?: string) => {
+  if (!code) {
+    return (
+      <Descriptions.Item label={intl.get('prescription.patient.identification.gestational.age')}>
+        {intl.get('prescription.patient.identification.foetus.dead')}
+      </Descriptions.Item>
+    );
+  }
+
+  const value =
+    code === DDM
+      ? calculateGestationalAgeFromDDM(new Date(valueDateTime))
+      : calculateGestationalAgeFromDPA(new Date(valueDateTime));
+  return (
+    <Descriptions.Item label={intl.get('prescription.patient.identification.gestational.age')}>
+      <Text>
+        {intl.get('prescription.patient.identification.simple.calculated.gestational.age', {
+          value,
+        })}
+      </Text>
+    </Descriptions.Item>
+  );
+};
+
+const GestationalInfo = (gestationalAgeObsReference: string, labelClass: string, code?: string) => {
+  const { gestationalDateObervationValue } = useGestationalDateObservationEntity(
+    extractObservationId(gestationalAgeObsReference),
+  );
+  return (
+    <>
+      <Divider />
+      <Descriptions column={1} size="small" className={labelClass}>
+        <Descriptions.Item label={intl.get('prescription.patient.identification.sexe.foetus')}>
+          {intl.get(`sex.${gestationalDateObervationValue?.focus.resource.gender}`)}
+        </Descriptions.Item>
+        {getGestationalAge(gestationalDateObervationValue?.valueDateTime, code)}
+      </Descriptions>
+    </>
+  );
+};
+
+const PatientContent = ({ patient, reference, isPrenatal, labelClass = 'label-35' }: OwnProps) => {
   let folder = <>{patient.mrn ?? EMPTY_FIELD}</>;
   if (reference) {
     folder = (
@@ -23,25 +72,57 @@ const PatientContent = ({ patient, reference, labelClass = 'label-35' }: OwnProp
     );
   }
 
+  let foetusInfo = <></>;
+
+  if (isPrenatal) {
+    const observation = patient.clinicalImpressions[0]?.investigation[0]?.item.find(
+      (i) => i.item?.code?.coding.code === DDM || i.item?.code?.coding.code === DPA,
+    );
+
+    if (observation) {
+      foetusInfo = GestationalInfo(
+        observation.reference,
+        labelClass,
+        observation.item?.code?.coding.code,
+      );
+    } else {
+      foetusInfo = (
+        <>
+          <Divider />
+          <Descriptions column={1} size="small" className={labelClass}>
+            <Descriptions.Item
+              label={intl.get('prescription.patient.identification.gestational.age')}
+            >
+              {intl.get('prescription.patient.identification.foetus.dead')}
+            </Descriptions.Item>
+          </Descriptions>
+        </>
+      );
+    }
+  }
+
   return patient ? (
-    <Descriptions column={1} size="small" className={labelClass}>
-      <Descriptions.Item label={intl.get('screen.prescription.entity.identifier')}>
-        {extractPatientId(patient.id)}
-      </Descriptions.Item>
-      <Descriptions.Item label={intl.get('screen.prescription.entity.patientContent.folder')}>
-        {folder}
-      </Descriptions.Item>
-      <Descriptions.Item label="RAMQ">{formatRamq(patient.person[0].ramq)}</Descriptions.Item>
-      <Descriptions.Item label={intl.get('name')}>
-        {formatName(patient.person[0].name[0])}
-      </Descriptions.Item>
-      <Descriptions.Item label={intl.get('birthdate')}>
-        {formatDate(patient.person[0].birthdate)}
-      </Descriptions.Item>
-      <Descriptions.Item label={intl.get('sex')}>
-        {intl.get(`sex.${patient.gender?.toLowerCase()}` ?? 'key')}
-      </Descriptions.Item>
-    </Descriptions>
+    <>
+      <Descriptions column={1} size="small" className={labelClass}>
+        <Descriptions.Item label={intl.get('screen.prescription.entity.identifier')}>
+          {extractPatientId(patient.id)}
+        </Descriptions.Item>
+        <Descriptions.Item label={intl.get('screen.prescription.entity.patientContent.folder')}>
+          {folder}
+        </Descriptions.Item>
+        <Descriptions.Item label="RAMQ">{formatRamq(patient.person[0].ramq)}</Descriptions.Item>
+        <Descriptions.Item label={intl.get('name')}>
+          {formatName(patient.person[0].name[0])}
+        </Descriptions.Item>
+        <Descriptions.Item label={intl.get('birthdate')}>
+          {formatDate(patient.person[0].birthdate)}
+        </Descriptions.Item>
+        <Descriptions.Item label={intl.get('sex')}>
+          {intl.get(`sex.${patient.gender?.toLowerCase()}` ?? 'key')}
+        </Descriptions.Item>
+      </Descriptions>
+      {foetusInfo}
+    </>
   ) : null;
 };
 
--- src/views/Prescriptions/Detail/PatientCard/index.tsx
+++ src/views/Prescriptions/Detail/PatientCard/index.tsx
@@ -11,15 +11,20 @@ interface OwnProps {
   loading: boolean;
 }
 
-const PatientCard = ({ prescription, loading }: OwnProps) => (
-  <Card title={intl.get('screen.prescription.entity.patient.card.title')} data-cy="PatientCard">
-    <ParagraphLoader loading={loading} paragraph={{ rows: 6 }}>
-      <PatientContent
-        patient={prescription?.subject.resource!}
-        reference={prescription?.requester?.organization?.reference}
-      />
-    </ParagraphLoader>
-  </Card>
-);
+const PatientCard = ({ prescription, loading }: OwnProps) => {
+  const patient = prescription?.subject.resource!;
+
+  return (
+    <Card title={intl.get('screen.prescription.entity.patient.card.title')} data-cy="PatientCard">
+      <ParagraphLoader loading={loading} paragraph={{ rows: 6 }}>
+        <PatientContent
+          patient={patient}
+          isPrenatal={prescription?.category[0]?.text === 'Prenatal'}
+          reference={prescription?.requester?.organization?.reference}
+        />
+      </ParagraphLoader>
+    </Card>
+  );
+};
 
 export default PatientCard;
