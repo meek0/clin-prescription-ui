31d3185360e00fa1ac6f201232f123cad86006f6
Karine St-Onge <kstonge@ferlab.bio>
2025-01-20 10:31:15 -0500

    feat: PRESC-341 Adjust Cypress tests

--- cypress/e2e/Consultation/TableauPrescriptions.cy.ts
+++ cypress/e2e/Consultation/TableauPrescriptions.cy.ts
@@ -9,18 +9,18 @@ beforeEach(() => {
 
 describe('Page des prescriptions - Consultation du tableau des prescriptions', () => {
   it('Vérifier les informations affichées', () => {
-    cy.get('tr[class*="ant-table-row"]').eq(0).find('[class="ant-table-cell"]').eq(0).contains(/\d{7}/).should('exist');
     cy.get('tr[class*="ant-table-row"]').eq(0).find('[class="ant-table-cell"]').eq(1).contains(/\d{7}/).should('exist');
-    cy.get('tr[class*="ant-table-row"]').eq(0).find('[class="ant-table-cell"]').eq(2).contains(/(Routine|ASAP)/).should('exist');
-    cy.get('tr[class*="ant-table-row"]').eq(0).find('[class="ant-table-cell"]').eq(3).contains('Soumise').should('exist');
-    cy.get('tr[class*="ant-table-row"]').eq(0).find('[class="ant-table-cell"]').eq(4).contains(/^\d{4}-\d{2}-\d{2}$/).should('exist');
-    cy.get('tr[class*="ant-table-row"]').eq(0).find('[class="ant-table-cell"]').eq(5).contains(/(DYSM|HYPM|MITN|MMG|MYAC|MYOC|POLYM|RGDI|RHAB)/).should('exist');
-    cy.get('tr[class*="ant-table-row"]').eq(0).find('[class="ant-table-cell"]').eq(6).contains(/(CHUSJ|CHUS|CUSM)/).should('exist');
+    cy.get('tr[class*="ant-table-row"]').eq(0).find('[class="ant-table-cell"]').eq(2).contains(/\d{7}/).should('exist');
+    cy.get('tr[class*="ant-table-row"]').eq(0).find('[class="ant-table-cell"]').eq(3).contains(/(Routine|ASAP)/).should('exist');
+    cy.get('tr[class*="ant-table-row"]').eq(0).find('[class="ant-table-cell"]').eq(4).contains('Soumise').should('exist');
+    cy.get('tr[class*="ant-table-row"]').eq(0).find('[class="ant-table-cell"]').eq(5).contains(/^\d{4}-\d{2}-\d{2}$/).should('exist');
+    cy.get('tr[class*="ant-table-row"]').eq(0).find('[class="ant-table-cell"]').eq(6).contains(/(DYSM|HYPM|MITN|MMG|MYAC|MYOC|POLYM|RGDI|RHAB)/).should('exist');
+    cy.get('tr[class*="ant-table-row"]').eq(0).find('[class="ant-table-cell"]').eq(7).contains(/(CHUSJ|CHUS|CUSM)/).should('exist');
   });
 
   it('Valider les liens disponibles Lien Prescription', () => {
     cy.intercept('POST', '**/$graphql*').as('getPOSTgraphql');
-    cy.get('tr[class*="ant-table-row"]').eq(0).find('[class="ant-table-cell"]').eq(0).find('a[href*="prescription"]').clickAndWait({force: true});
+    cy.get('tr[class*="ant-table-row"]').eq(0).find('[class="ant-table-cell"]').eq(1).find('a[href*="prescription"]').clickAndWait({force: true});
     cy.wait('@getPOSTgraphql');
 
     cy.contains('Identifiant').should('exist');
@@ -30,56 +30,56 @@ describe('Page des prescriptions - Consultation du tableau des prescriptions', (
 describe('Page des prescriptions - Consultation du tableau des prescriptions', () => {
   it('Valider les fonctionnalités du tableau - Tri Prescription', () => {
     cy.sortTableAndIntercept('Prescription', 1);
-    cy.validateTableFirstRow(/\d{7}/, 0);
+    cy.validateTableFirstRow(/\d{7}/, 1);
     cy.sortTableAndIntercept('Prescription', 1);
-    cy.validateTableFirstRow(/\d{7}/, 0);
+    cy.validateTableFirstRow(/\d{7}/, 1);
   });
 
   it('Valider les fonctionnalités du tableau - Tri Patient', () => {
     cy.sortTableAndIntercept('Patient', 1);
-    cy.validateTableFirstRow(/\d{7}/, 1);
+    cy.validateTableFirstRow(/\d{7}/, 2);
     cy.sortTableAndIntercept('Patient', 1);
-    cy.validateTableFirstRow(/\d{7}/, 1);
+    cy.validateTableFirstRow(/\d{7}/, 2);
   });
 
   it('Valider les fonctionnalités du tableau - Tri Priorité', () => {
     cy.sortTableAndIntercept('Priorité', 1);
-    cy.validateTableFirstRow(/^(-|Routine|ASAP)$/, 2);
+    cy.validateTableFirstRow(/^(-|Routine|ASAP)$/, 3);
     cy.sortTableAndIntercept('Priorité', 1);
-    cy.validateTableFirstRow(/^(Routine|ASAP)$/, 2);
+    cy.validateTableFirstRow(/^(Routine|ASAP)$/, 3);
   });
 
   it('Valider les fonctionnalités du tableau - Tri Statut', () => {
     cy.sortTableAndIntercept('Statut', 1);
-    cy.validateTableFirstRow('Soumise', 3);
+    cy.validateTableFirstRow('Soumise', 4);
     cy.sortTableAndIntercept('Statut', 1);
-    cy.validateTableFirstRow('Soumise', 3);
+    cy.validateTableFirstRow('Soumise', 4);
   });
 
   it('Valider les fonctionnalités du tableau - Tri Créée le', () => {
     cy.sortTableAndIntercept('Créée le', 1);
-    cy.validateTableFirstRow(/^\d{4}-\d{2}-\d{2}$/, 4);
+    cy.validateTableFirstRow(/^\d{4}-\d{2}-\d{2}$/, 5);
     cy.sortTableAndIntercept('Créée le', 1);
-    cy.validateTableFirstRow(/^\d{4}-\d{2}-\d{2}$/, 4);
+    cy.validateTableFirstRow(/^\d{4}-\d{2}-\d{2}$/, 5);
   });
 
   it('Valider les fonctionnalités du tableau - Tri Analyse', () => {
     cy.sortTableAndIntercept('Analyse', 1);
-    cy.validateTableFirstRow(/(DYSM|HYPM|MITN|MMG|MYAC|MYOC|POLYM|RGDI|RHAB)/, 5);
+    cy.validateTableFirstRow(/(DYSM|HYPM|MITN|MMG|MYAC|MYOC|POLYM|RGDI|RHAB)/, 6);
     cy.sortTableAndIntercept('Analyse', 1);
-    cy.validateTableFirstRow(/(DYSM|HYPM|MITN|MMG|MYAC|MYOC|POLYM|RGDI|RHAB)/, 5);
+    cy.validateTableFirstRow(/(DYSM|HYPM|MITN|MMG|MYAC|MYOC|POLYM|RGDI|RHAB)/, 6);
   });
 
   it('Valider les fonctionnalités du tableau - Tri EP', () => {
     cy.sortTableAndIntercept('EP', 1);
-    cy.validateTableFirstRow(/(CHUSJ|CHUS|CUSM)/, 6);
+    cy.validateTableFirstRow(/(CHUSJ|CHUS|CUSM)/, 7);
     cy.sortTableAndIntercept('EP', 1);
-    cy.validateTableFirstRow(/(CHUSJ|CHUS|CUSM)/, 6);
+    cy.validateTableFirstRow(/(CHUSJ|CHUS|CUSM)/, 7);
   });
 
   it('Valider les fonctionnalités du tableau - Tri multiple', () => {
     cy.sortTableAndIntercept('EP', 1);
     cy.sortTableAndIntercept('Analyse', 1);
-    cy.validateTableFirstRow(/\d{7}/, 0);
+    cy.validateTableFirstRow(/\d{7}/, 1);
   });
 });
