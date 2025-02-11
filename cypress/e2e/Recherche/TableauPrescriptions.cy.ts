de892772c88658879c7276941764324e3ee48a46
kstonge <karine.st-onge.hsj@ssss.gouv.qc.ca>
2025-02-11 11:03:48 -0500

    feat: PRESC-376 Adjust Cypress tests

--- cypress/e2e/Recherche/TableauPrescriptions.cy.ts
+++ cypress/e2e/Recherche/TableauPrescriptions.cy.ts
@@ -11,7 +11,7 @@ beforeEach(() => {
   cy.resetColumns(0);
 });
 
-describe('Page des prescriptions - Rechercher des prescriptions [PRESC-376]', () => {
+describe('Page des prescriptions - Rechercher des prescriptions', () => {
   it('Par numéro de prescription', () => {
     cy.typeAndIntercept('[class*="PractitionerTable_patientContentContainer"] [class="ant-input"]', epCHUSJ_ldmCHUSJ.prescriptionId, 'POST', '**/graphql', 1);
 
@@ -42,34 +42,6 @@ describe('Page des prescriptions - Rechercher des prescriptions [PRESC-376]', ()
 
   it('Par nom du cas-index', () => {
     cy.typeAndIntercept('[class*="PractitionerTable_patientContentContainer"] [class="ant-input"]', epCHUSJ_ldmCHUSJ.lastNameProb, 'POST', '** /graphql', 1);
-
-    cy.get('tr[class*="ant-table-row"]').eq(1).should('not.exist');
-    cy.validateTableFirstRow(epCHUSJ_ldmCHUSJ.prescriptionId, 1);
-  });
-
-  it('Par numéro de dossier de la mère', () => {
-    cy.typeAndIntercept('[class*="PractitionerTable_patientContentContainer"] [class="ant-input"]', epCHUSJ_ldmCHUSJ.mrnMth.toLowerCase(), 'POST', '** /graphql', 1);
-
-    cy.get('tr[class*="ant-table-row"]').eq(1).should('not.exist');
-    cy.validateTableFirstRow(epCHUSJ_ldmCHUSJ.prescriptionId, 1);
-  });
-
-  it('Par numéro de ramq de la mère', () => {
-    cy.typeAndIntercept('[class*="PractitionerTable_patientContentContainer"] [class="ant-input"]', epCHUSJ_ldmCHUSJ.ramqMth, 'POST', '** /graphql', 1);
-
-    cy.get('tr[class*="ant-table-row"]').eq(1).should('not.exist');
-    cy.validateTableFirstRow(epCHUSJ_ldmCHUSJ.prescriptionId, 1);
-  });
-
-  it('Par prénom de la mère', () => {
-    cy.typeAndIntercept('[class*="PractitionerTable_patientContentContainer"] [class="ant-input"]', epCHUSJ_ldmCHUSJ.firstNameMth, 'POST', '** /graphql', 1);
-
-    cy.get('tr[class*="ant-table-row"]').eq(1).should('not.exist');
-    cy.validateTableFirstRow(epCHUSJ_ldmCHUSJ.prescriptionId, 1);
-  });
-
-  it('Par nom de la mère', () => {
-    cy.typeAndIntercept('[class*="PractitionerTable_patientContentContainer"] [class="ant-input"]', epCHUSJ_ldmCHUSJ.lastNameMth, 'POST', '** /graphql', 1);
 
     cy.get('tr[class*="ant-table-row"]').eq(1).should('not.exist');
     cy.validateTableFirstRow(epCHUSJ_ldmCHUSJ.prescriptionId, 1);
