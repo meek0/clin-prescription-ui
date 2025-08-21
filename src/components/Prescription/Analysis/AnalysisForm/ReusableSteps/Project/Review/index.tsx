2bc90147b78233e6d0110dbc0e01bdb8372ab1bd
claudia1296 <claudia.roy.cr@gmail.com>
2025-08-21 13:04:16 -0400

    feat: CLIN-4975 fix review section

--- src/components/Prescription/Analysis/AnalysisForm/ReusableSteps/Project/Review/index.tsx
+++ src/components/Prescription/Analysis/AnalysisForm/ReusableSteps/Project/Review/index.tsx
@@ -1,11 +1,7 @@
 import intl from 'react-intl-universal';
 import { Descriptions } from 'antd';
-import { isEmpty } from 'lodash';
 
-import {
-  EMPTY_FIELD,
-  STEPS_ID,
-} from 'components/Prescription/Analysis/AnalysisForm/ReusableSteps/constant';
+import { STEPS_ID } from 'components/Prescription/Analysis/AnalysisForm/ReusableSteps/constant';
 import EmptySection from 'components/Prescription/components/EmptySection';
 import { IProjectDataType } from 'components/Prescription/components/Project/types';
 import { usePrescriptionForm } from 'store/prescription';
@@ -14,15 +10,10 @@ const ProjectReview = () => {
   const { analysisFormData } = usePrescriptionForm();
   const getData = (key: keyof IProjectDataType) => analysisFormData[STEPS_ID.PROJECT]?.[key];
 
-  const getResearchProject = () => {
-    const project = getData('project' satisfies keyof IProjectDataType);
-    return isEmpty(history) ? EMPTY_FIELD : project;
-  };
-
   return getData('project' satisfies keyof IProjectDataType) ? (
     <Descriptions className="label-20" column={1} size="small">
       <Descriptions.Item label={intl.get('prescription.project.select.label')}>
-        {getResearchProject()}
+        {getData('project' satisfies keyof IProjectDataType)}
       </Descriptions.Item>
     </Descriptions>
   ) : (
